dfce4b0408bdf71f0355d171ecd9a4034b517886
VankoID <dudarev.id@gmail.com>
2025-02-28 18:14:26 +0400

    added brain-progression

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/9a9e60aa7b0fc6bbce1b/maintainability)](https://codeclimate.com/github/VankoID/frontend-project-44/maintainability)
 
-Game brain-even https://asciinema.org/a/KfW87cuzJQcvjQsij1zlG3SH2
 
-Game brain-calc https://asciinema.org/a/fa36DP9UqeNhTQI1zP4RwIuLj
+Asciinema's for games:
 
-Game brain-gcd https://asciinema.org/a/z0RK9HjfDW3IWWNfUtvHu19PK
+brain-even https://asciinema.org/a/KfW87cuzJQcvjQsij1zlG3SH2
+brain-calc https://asciinema.org/a/fa36DP9UqeNhTQI1zP4RwIuLj
+brain-gcd https://asciinema.org/a/z0RK9HjfDW3IWWNfUtvHu19PK
+brain-progression :https://asciinema.org/a/Vf3nKBI2ZmrYlR8tENBuQ80Zs
---
+++ bin/brain-progression.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+
+const brain_progression = () => {        
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name: ');
+    console.log(`Hello, ${name}!`)
+    let i = 0;
+    while (i < 3) { 
+        let number = Math.floor(Math.random() * 100 + 1);
+        let diff = Math.floor(Math.random() * 10 + 1);    
+        let j = 0;
+        let array = [];
+        while (j < 11) {
+            number = number + diff;
+            j = j + 1; 
+            array.push(number);
+        }
+        let randomIndex = Math.floor(Math.random() * (array.length - 1));
+        let checkRandom = (array[randomIndex]);    
+        // console.log(array[randomIndex]);
+        array[randomIndex] = '..';
+        let str = array.join(' ');
+        console.log('What number is missing in the progression?');
+        console.log(`Question: ${str}`);
+        const answer = readlineSync.question('Your answer: ');
+        if (Number(answer) === checkRandom) {
+            i = i + 1;
+            console.log('Correct');
+    } else return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkRandom}'.\nLet's try again, ${name}!`)
+} return console.log(`Congratulation, ${name}`)
+}
+brain_progression();
