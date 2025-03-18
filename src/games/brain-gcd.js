c8e0a942486bdcad2563a71abb421d653164c2ac
VankoID <dudarev.id@gmail.com>
2025-03-18 11:52:08 +0400

    code refactoring game: brain-gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,28 +1,27 @@
-import { gretting, randomNumber } from '../index.js';
+import { randomNumber, gretting, mainGame } from '../index.js';
 
 const brainGcd = () => {
-  let i = 0;
-  const name = gretting();
-  console.log('Find the greatest common divisor of given numbers.');
-  while (i < 3) {
+  const generateQuestion = () => {
     let number = randomNumber();
     let number2 = randomNumber();
-    console.log(`Question: ${number} ${number2}`);
-    const answer = readlineSync.question('Your answer: ');
+    const savedNumber = number;
+    const savedNumber2 = number2;
     while (number !== number2) {
       if (number > number2) {
         number -= number2;
       }
       if (number2 > number) {
         number2 -= number;
-      }
-    }
-    const solution = number;
-    if (Number(answer) === solution) {
-      i += 1;
-      console.log('Correct');
-    } else return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${solution}'.\nLet's try again, ${name}!`);
-  } return console.log(`Congratulations, ${name}!`);
+      };
+    };
+    const correctAnswer = number;
+    return {
+      question: `Question: ${savedNumber} ${savedNumber2}`,
+      correctAnswer: String(correctAnswer),
+    };
+  };
+  const name = gretting();
+  console.log('Find the greatest common divisor of given numbers.');
+  mainGame(name, generateQuestion);
 };
-
 export default brainGcd;
